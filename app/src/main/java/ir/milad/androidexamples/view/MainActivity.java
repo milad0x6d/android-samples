ef80c0f89f116bd7f82585d0966efdabda017c35
milad <miladnorozii33@gmail.com>
2020-03-14 19:09:52 +0330

    mvvm-bindingadapter added

---
+++ app/src/main/java/ir/milad/androidexamples/view/MainActivity.java
@@ -0,0 +1,32 @@
+package ir.milad.androidexamples.view;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.BindingAdapter;
+import androidx.databinding.DataBindingUtil;
+
+import ir.milad.androidexamples.R;
+import ir.milad.androidexamples.databinding.ActivityMainBinding;
+import ir.milad.androidexamples.viewmodel.LoginViewModel;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        ActivityMainBinding activityMainBinding =
+                DataBindingUtil.setContentView(this,R.layout.activity_main);
+        activityMainBinding.setViewModel(new LoginViewModel());
+        activityMainBinding.executePendingBindings();
+    }
+
+    @BindingAdapter({"toastMessage"})
+    public static void runMe(View view , String message){
+        if(message != null)
+            Toast.makeText(view.getContext(), message, Toast.LENGTH_SHORT).show();
+    }
+}
